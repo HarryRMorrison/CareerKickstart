60ad3fab46fae9e7cd3e30cc9adba49b926f3112
harryM24 <144313934+harryM24@users.noreply.github.com>
2024-04-26 15:58:17 +0800

    Setup first page explore page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,153 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/explore')
+def load_explorepage():
+    posts = [
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        },
+        {
+            'title': "Hello dfgdrgnoerngoinoinger",
+            'description': 'fgieorigneirngoienrgoenrgneorigoekrngoer igoierg oierjgoiehrgoiehr goierh goiehrgoiehrg oeihr goi Beautiful day in Portland!',
+            'tags': ["Accounting","Internship","Finance","Deloitte","Interviews"],
+            'likes': 4,
+            'comments': 1 
+        }
+    ]
+    return render_template("explorePage.html", posts=posts)
