4cd3879aa96155e4c68c23192abce87046095840
harryM24 <144313934+harryM24@users.noreply.github.com>
2024-04-27 17:20:19 +0800

    Added the basic files for the database setup
    Note: Had to reinstall the venv as the sqlalchemy package was not being recognised.

---
+++ app/models.py
@@ -0,0 +1,9 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Harry\OneDrive - The University of Western Australia\2024 Sem1\Agile\project\agile-project\venv
+set VIRTUAL_ENV=C:\Users\Harry\Documents\GitHub\CareerKickstart\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
