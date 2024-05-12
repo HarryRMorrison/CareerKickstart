a778e338ae8753527facdffb179b99c9cc99b96d
EmilyRuii <em8ong@gmail.com>
2024-05-12 19:22:03 +0800

    Adding Signup and Login connection
    This code is to add the signup and login connection, but when I test it by putting in login/signup details, it doesn't work and goes to an error page - does anyone know how to test this?

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from sqlalchemy.sql import func
 from app import db
+from werkzeug.security import generatePasswordHash, checkPasswordHash #Emily-add
 
 class Tag(db.Model):
     __tablename__ = 'tags'
@@ -17,12 +18,18 @@ class Tag(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(30))
-    email = db.Column(db.String(100))
-    password = db.Column(db.String(50))
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_Hash = db.Column(db.String(50), nullable=False)
 
     user_questions = db.relationship('Question', back_populates='user', lazy=True)
     user_answers = db.relationship('Answer', back_populates='user', lazy=True)
+
+    def setPword(self, password):
+        self.password_Hash = generatePasswordHash(password)
+
+    def checkPword(self, password):
+        return checkPasswordHash(self.password_Hash, password)
 
     def __repr__(self):
         return '[<User {}> <User_id {}> <Email {}>]'.format(self.username, self.user_id, self.email)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -15,3 +16,28 @@ def send_card_template():
 def load_explorepage():
     posts = []
     return render_template("explorePage.html", posts=posts)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if User.query.filter((User.username == username) | (User.email == email)).first():
+        return jsonify({"error": "This username or email already exists."}), 400
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "You have been registered."}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username_email = data.get('username_email')
+    password = data.get('password')
+    user = User.query.filter((User.username == username_email) | (User.email == username_email)).first()
+    if user and user.check_password(password):
+        return jsonify({"message": "Logged in."}), 200
+    return jsonify({"error": "Entered credentials are invalid."}), 401
