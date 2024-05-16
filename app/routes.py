79205fa10aa3644e601c6e37eb860c83ca1477e1
HarryRMorrison <123150981+HarryRMorrison@users.noreply.github.com>
2024-05-16 13:20:31 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,20 @@ from app.forms import LoginForm, SignupForm
 from app.controller import PostController
 
 @app.route('/')
-@app.route('/index')
+@app.route('/home')
 def index():
-    return "Hello, World!"
+    return render_template('home.html')
+
+@app.route('/explore', methods=['GET'])
+def load_explorepage():
+    arguments = request.args
+    if len(arguments) > 0:
+        if len(arguments) == 1 and arguments.get('query', '') == '':
+            return PostController.get_top_questions()
+        else:
+            return PostController.get_searched_questions(arguments)
+    else:
+        return PostController.get_top_questions()
 
 @app.route('/getcard', methods = ['GET'])
 def send_card_template():
@@ -21,11 +32,7 @@ def load_createpage():
 
 @app.route('/login', methods=['GET','POST'])
 def load_login():
-    return render_template('login.html')    
-    if request.method == 'POST':
-        return PostController.create_post()
-    else:
-        return render_template("create.html")
+    return render_template('login.html')  
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -54,3 +61,18 @@ def login():
         if user and user.check_password(password):
             return jsonify({"message": "Logged in."}), 200
     return jsonify({"error": "Entered credentials are invalid."}), 401
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
